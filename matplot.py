d16b495fd222a270ee8be55804031c59d5a029a1
SHAYYAN <127933448+mohdshayyan@users.noreply.github.com>
2023-09-29 07:06:40 -0700

    matplot.py

--- matplot.py
+++ matplot.py
@@ -1,65 +1,89 @@
-import matplotlib.pyplot as plt
-month=['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
-units=[12,11,14,10,13,15,17,13,14,17,12,11]
-# Plotting a line chart: 
-plt.plot(month,units,marker='D',linestyle='dashed',color='red') #or 'dotted'
-# Modifying:
-plt.xlabel('Months Names')
-plt.xticks(month)
-plt.ylabel('Units in Lakhs')
-plt.yticks(units)
-plt.title('Month-wise Production')
-plt.grid(True)
-#plt.legend()
-plt.show()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-'''
-data={'A':[30,40,50,60,78,58,74],'B':[45,78,58,74,78,58,74],'C':[6000,6700,6250,7520,78,58,74],'E':[6000,6700,6250,7570,78,58,74],'F':[6000,6700,6250,7525,78,58,74],'G':[6000,6700,6250,7740,78,58,74]}
-cdf=pd.DataFrame(data)
-
-#print(cdf)
-print(cdf.max())# column
-print('--------------------------------------')
-print(cdf.max(axis=1))# Row
-'''
-'''
 import pandas as pd
-names = ['shruti', 'renu', 'rinku', 'nidhi', 'tinku', 'Rohit']
-marks = [80, 60, 89, 90, 78, 99]
-st = pd.Series(marks, index=names)
+import numpy as np
+
+#--------------------------------------------------------- Series -------------------------------------------------------------------
+#s1=pd.Series()
+#print(s1)
 '''
-#print(st['Renu']=70)
-#st=st['renu']+10
+df=pd.DataFrame({'Age':[36,40,35],'Desg':['Manager','Clerk','Accountant']},index=['Shaha','Parth','Neha'])
+df=index.name('Name')
+print(df)
+'''
+#df=pd.DataFrame({'Green':[2,'F'],'Red':[1,6],'Yellow':[3,7]})
+#print(df)
+#x=pd.Series([10,20,30],index=['kam','Jam','tam'])#(['kam','Jam','tam'],index=[10,20,30])     #,dtype=np.object_)
+# tam ko 35 pr:-->
+#x['tam']=35
+# Modify full index in series:-->
+#x.index=['k','J','t']
+#print(x)
+# Slicing:-->
+#x=x[x>10]
+#print(x)
+'''
+df[20]=25
+df=df.T
+df=df.drop(10,axis=0)
+df[25]='Jam'
+print(df)
+'''
+#Naming index:-->
+#x.index.name='Name'
+#x.name='Result'
+#print(x)
 
-#print(st)
-#print(st[st> 85])
+#--------------------------------------------------------- DataFrame -------------------------------------------------------------------
+#Dataframe from dict of series :
+'''
+s1=pd.Series(['A','B'])
+s2=pd.Series(['C','D'])
+df=pd.DataFrame({'A1':s1,'A2':s2})
+print(df)
+'''
+#DataFrame from List of Dictionaries
 
-#st['renu'] += 10
-#print(st)
-
-
-#print(st.drop(['tinku']))
-
-
-
-
-
-
-
+'''
+l=[{'Name':'A','Sr':'a'},{'Name':'B','Sr':'b'},{'Name':'C','Sr':'c'}]
+df=pd.DataFrame(l)
+print(df)
+'''
+# To Access Single Row
+'''
+idx=['Sub1','Sub2','Sub3']
+data={'A':[1,2,3],'B':[4,5,6],'C':[7,8,9]}
+df=pd.DataFrame(data,index=idx)
+print(df)
+print(df.loc[:,:'A'])
+'''
+#Sort values in df:-->
+'''
+data={'Age':[2,1,3,5,4]}
+df=pd.DataFrame(data)
+df=df.sort_values(by='Age')
+print(df)
+'''
+# Add a Column in df:-->#df['D']=[10,11,12]
+#df['D']=20
+#--------------------------------------------------
+# Add a Row in df:-->
+#df.loc['Sub4']=[10,11,12]
+#--------------------------------------------------
+# Drop a Column in df:-->
+#1) df.pop('C')
+#2) del df['C']
+#3) df=df.drop(['C'],axis=1)
+#--------------------------------------------------
+# Drop a Row in df:-->
+#df=df.drop(['Sub3'],axis=0)
+#----------------------------------------------------------------------------------------
+# Rename a Column in df:-->#df=df.rename({'A':'A1'},axis='columns')
+#-----------------------------------------------------------------------------------------
+'''
+s = pd.Series([10,15,18,22])
+df=pd.DataFrame(s)
+df.columns=['List1']
+'''
+# Rename a Row(Index) in df:-->
+#df=df.rename({'Sub3':'S3'})#,axis='index')
+###print(df)
+#-----------------------------------------------------------------------------------------
